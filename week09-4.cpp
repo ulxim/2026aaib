3385b26d07a80a94fce9b640ff6211ee6f27daed
ulxim <eileench9@gmail.com>
2026-04-22 11:45:50 +0800

    week09

---
+++ week09-4.cpp
@@ -0,0 +1,10 @@
+///week09-4.cpp 要交換陣列裡的數字
+#include <stdio.h>
+int main()
+{
+    int a=99,b=34;
+    printf("一開始a:%d b:%d\n",a,b);
+    int temp =a;
+    a=b;
+    printf("交換後 a:%d b:%d\n",a,b);
+}
---
+++ week09-6.cpp
@@ -0,0 +1,22 @@
+///week09-6.cpp 想要更了解大到小的排序
+#include <stdio.h>
+int main()
+{
+    int a[10]={11,22,33,44,55,66,77,88,99,97};
+
+    for (int i=0;i<10;i++)printf("%d ",a[i]);
+    printf("\n");
+
+    for (int k=0;k<20;k++){
+        for(int i=0;i<10-1;i++){
+           if(a[i]<a[i+1]){
+             int temp =a[i];
+             a[i]=a[i+1];
+             a[i+1]=temp;
+        }
+    }
+    for(int i=0;i<10;i++)printf("%d ",a[i]);
+    printf("\n");
+    }
+}
+
